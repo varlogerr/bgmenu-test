a8ef4e591704543c85d5bb6afd5114bc567bf18e
Varlog <varlog.err@gmail.com>
2016-11-27 10:41:40 +0000

    add get all to user api object

--- public/js/bgmenu/user.js
+++ public/js/bgmenu/user.js
@@ -1,6 +1,17 @@
 var bgmenu = (function (bgmenu, $) {
     bgmenu.user = (function (user) {
-
+        user.getAll = function () {
+            $.ajax('/users', {
+                dataType : 'json',
+                contentType : 'application/json; utf-8'
+            })
+                .done(function (data) {
+                    console.log(data);
+                })
+                .fail(function (error) {
+                    // TODO
+                });
+        };
 
         return user;
     }(bgmenu.user || {}));
