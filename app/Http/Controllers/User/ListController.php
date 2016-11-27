d61f10dc0eb164c408f2ca4f4546f00d322c22c2
Varlog <varlog.err@gmail.com>
2016-11-27 15:35:14 +0000

    add quick and dirty list users

--- app/Http/Controllers/User/ListController.php
+++ app/Http/Controllers/User/ListController.php
@@ -4,15 +4,8 @@ namespace App\Http\Controllers\User;
 
 class ListController extends \App\Http\Controllers\Controller  {
     public function __invoke() {
-        return [
-            [
-                'name' => 'dummy',
-                'age' => 20,
-            ],
-            [
-                'name' => 'helloworld',
-                'age' => 'immortal',
-            ],
-        ];
+        $users = \App\User::all();
+
+        return $users;
     }
 }
