71a0989ee748088a494ec67e9bc053c0ce676c83
Varlog <varlog.err@gmail.com>
2016-11-28 11:05:25 +0000

    use user create validator in create user service

--- app/Bgmenu/Services/User/CreateService.php
+++ app/Bgmenu/Services/User/CreateService.php
@@ -3,11 +3,19 @@
 namespace App\Bgmenu\Services\User;
 
 class CreateService {
+    private $userCreateValidator;
+
+    public function __construct(CreateValidation $userCreateValidator) {
+        $this->userCreateValidator = $userCreateValidator;
+    }
+
     /**
      * @param array $data
      * @return \App\User
      */
     public function process(array $data) {
+        $this->userCreateValidator->process($data);
+
         return \App\User::create($data);
     }
 }
