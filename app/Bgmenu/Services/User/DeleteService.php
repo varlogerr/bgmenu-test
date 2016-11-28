e8935040cb7ff470678f5cd8e42f5c106e101e29
Varlog <varlog.err@gmail.com>
2016-11-28 13:17:51 +0000

    fix input param

--- app/Bgmenu/Services/User/DeleteService.php
+++ app/Bgmenu/Services/User/DeleteService.php
@@ -5,7 +5,7 @@ namespace App\Bgmenu\Services\User;
 
 class DeleteService {
     public function process($id) {
-        $user = \App\User::find(\Request::input('id'));
+        $user = \App\User::find($id);
         if (! $user) {
             return null;
         }
