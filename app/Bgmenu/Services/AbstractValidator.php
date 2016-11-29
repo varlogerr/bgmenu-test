a0976a665d441341e6eb04f8954f47b05da9f0c7
Varlog <varlog.err@gmail.com>
2016-11-29 19:53:25 +0000

    add after validation callback

--- app/Bgmenu/Services/AbstractValidator.php
+++ app/Bgmenu/Services/AbstractValidator.php
@@ -10,8 +10,19 @@ abstract class AbstractValidator {
         $this->config = $config;
     }
 
+    /**
+     * Get after validation callback
+     *
+     * @return \Closure|null
+     */
+    public function getAfter($data) { return null; }
+
     public function process($data) {
         $validator = \Validator::make($data, $this->config);
+
+        if (($after = $this->getAfter($data))) {
+            $validator->after($after);
+        }
 
         if ($validator->fails()) {
             throw new \App\Bgmenu\Exceptions\ValidationException($this->getErrorMessage(), $this->getErrorCode(), null, $validator->errors()->messages());
