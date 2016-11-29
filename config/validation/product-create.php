918b7a5395e95f39eca1390a33543390e372262c
Varlog <varlog.err@gmail.com>
2016-11-29 15:10:50 +0000

    fix product amount validation

--- config/validation/product-create.php
+++ config/validation/product-create.php
@@ -4,7 +4,7 @@ return [
     'name'           => 'required|max:255',
     'description'    => 'required|max:10000',
     'price'          => 'required|regex:/^\d{1,7}\.\d{2}$/',
-    'amount'         => 'required|integer',
+    'amount'         => 'required|integer|min:0',
     'available_from' => 'required|date|date_format:Y-m-d|after:yesterday',
     'available_to'   => 'required|date|date_format:Y-m-d|after:available_from',
 ];
--- config/validation/product-update.php
+++ config/validation/product-update.php
@@ -4,7 +4,7 @@ return [
     'name'           => 'max:255',
     'description'    => 'max:10000',
     'price'          => 'regex:/^\d{1,7}\.\d{2}$/',
-    'amount'         => 'integer',
+    'amount'         => 'integer|min:0',
     'available_from' => 'date|date_format:Y-m-d|after:yesterday',
     'available_to'   => 'date|date_format:Y-m-d|after:available_from',
 ];
