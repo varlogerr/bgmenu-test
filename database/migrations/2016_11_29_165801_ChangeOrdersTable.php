f7b0d81ba47e9a5fad852e5424aa603c95da1f4a
Varlog <varlog.err@gmail.com>
2016-11-29 19:51:16 +0000

    fix seeding

---
+++ database/migrations/2016_11_29_165801_ChangeOrdersTable.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('orders_user_id_foreign');
+            $table->dropColumn('user_id');
+            $table->string('name')->nullable()->change();
+            $table->text('description')->nullable()->change();
+            $table->unsignedInteger('price')->nullable()->change();
+
+            $table->foreign('cart_id')
+                ->references('id')
+                ->on('carts')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // No
+    }
+}
