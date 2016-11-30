accfd9e2ce8e307fd4d0897c7d697f08ce91c946
Varlog <varlog.err@gmail.com>
2016-11-30 08:42:27 +0000

    add price with vat virtual field to order

--- app/Order.php
+++ app/Order.php
@@ -3,6 +3,8 @@
 namespace App;
 
 class Order extends \Illuminate\Database\Eloquent\Model {
+    private static $vat;
+
     protected $fillable = [
         'product_id', 'cart_id', 'name', 'description', 'price', 'quantity',
     ];
@@ -13,5 +15,13 @@ class Order extends \Illuminate\Database\Eloquent\Model {
 
     public function product() {
         return $this->belongsTo(Product::class);
+    }
+
+    public function getPriceWithVatAttribute() {
+        if (! static::$vat) {
+            static::$vat = Setting::where('key', 'vat_percent')->first()->value;
+        }
+
+        return $this->price + ($this->price * static::$vat / 100);
     }
 }
